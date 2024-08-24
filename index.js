e699f9bea14f5aa9f0ba0bc87cad3db9921cf588
Daniel Kelvi da Silva <siechi2012alt@gmail.com>
2024-08-24 19:39:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-torrar("pao de forma" , "Siechi")
-torrar("pao de forma" , "Nitsoo")
+torrar("pao de forma", "underfined", "Nitsoo", )
 
-function torrar(pao , nome){
+function torrar(pao, valor=727, nome = "Cliente"){
     console.log("torrada feita com " + pao)
-    console.log("ela é um pedido de " + nome)
+    console.log("ela é um pedido de um " + nome)
+    console.log("o valor total é " + valor)
 }
