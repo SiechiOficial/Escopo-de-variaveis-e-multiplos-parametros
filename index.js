67f570f5c799fdf11fb923dada7c6c3ba2387a83
Daniel Kelvi da Silva <siechi2012alt@gmail.com>
2024-08-24 19:22:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.error();
+torrar("pao de forma" , "Siechi")
+torrar("pao de forma" , "Nitsoo")
+
+function torrar(pao , nome){
+    console.log("torrada feita com " + pao)
+    console.log("ela é um pedido de " + nome)
+}
